691e08119a5f3f91ab54c89b6c0c955f0694b5e1
RantoUzumaki <rberk2011@gmail.com>
2025-05-21 06:32:47 +0000

    home layout fix

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,6 +1,6 @@
 const Home = () => {
   return (
-    <div className="flex w-full h-full justify-between items-center px-60">
+    <div className="flex w-full h-full justify-between items-center">
       <div className="flex w-1/2 h-full justify-end items-center">
         <div className="flex flex-col h-max">
           <span className="text-normal text-darkWhite w-full text-start">
@@ -32,9 +32,13 @@ const Home = () => {
           </div>
         </div>
       </div>
-      <div className="w-1/2">
-        <div className="w-[550px] h-[550px] bg-green absolute left-[45%] top-[10%] rotate-45 z-[2] opacity-40 blur-[87px] " />
-        <div className="w-[550px] h-[550px] bg-violet absolute right-[5%] bottom-[5%] rotate-[80deg] z-[2] opacity-40 blur-[87px]" />
+      <div className="w-1/2 relative h-full">
+        <div className="w-[300px] h-[300px] bg-green absolute left-[0%] top-[0%] rotate-45 z-[2] opacity-40 blur-[87px] " />
+        <div className="w-[400px] h-[400px] bg-green absolute left-[5%] top-[25%] rotate-60 z-[2] opacity-40 blur-[87px] " />
+        <div className="w-[500px] h-[500px] bg-green absolute left-[40%] top-[10%] rotate-75 z-[2] opacity-40 blur-[87px] " />
+        <div className="w-[300px] h-[300px] bg-violet absolute right-[0%] bottom-[0%] rotate-45 z-[2] opacity-40 blur-[87px]" />
+        <div className="w-[400px] h-[400px] bg-violet absolute right-[5%] bottom-[25%] rotate-60 z-[2] opacity-40 blur-[87px]" />
+        <div className="w-[500px] h-[500px] bg-violet absolute right-[40%] bottom-[10%] rotate-75 z-[2] opacity-40 blur-[87px]" />
       </div>
     </div>
   );
