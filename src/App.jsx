bf9e7963b5f2e221c6127f547d3828c302d93efb
RantoUzumaki <rberk2011@gmail.com>
2025-05-21 04:39:06 +0000

    up

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,18 @@
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import HeaderNav from './components/HeaderNav';
-import Home from './pages/Home';
-import AboutMe from './pages/AboutMe';
-import Projects from './pages/Projects';
-import ContactMe from './pages/ContactMe';
-import FooterNav from './components/FooterNav';
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import HeaderNav from "./components/HeaderNav";
+import Home from "./pages/Home";
+import AboutMe from "./pages/AboutMe";
+import Projects from "./pages/Projects";
+import ContactMe from "./pages/ContactMe";
+import "./index.css";
+import FooterNav from "./components/FooterNav";
 
 function App() {
   return (
     <div className="bg-darkBlue rounded-default border border-border relative w-container h-container font-body">
       <Router>
         <HeaderNav />
-        <div className='w-full h-content'>
+        <div className="w-full h-content">
           <Routes>
             <Route exact path="/" element={<Home />} />
             <Route path="/about-me" element={<AboutMe />} />
